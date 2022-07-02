6a269e61d09fe871a4042c5a49fa0582c2f34c73
Songwit Phitakjariyachat <zsongwit.p@pttdigital.com>
2022-07-02 22:03:42 +0700

    Add project files.

---
+++ BinaryGap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    public static class BinaryGap
+    {
+        public static int Solution(int N)
+        {
+
+            int answer = 0;
+            string binary = Convert.ToString(N, 2);
+            int bilen = binary.Length;
+            int maxlen = 0;
+            Char[] ch = binary.ToCharArray();
+
+
+            for (int i = 0; i <= bilen - 1; i++)
+            {
+
+                if (ch[i].ToString() == "0")
+                {
+                    maxlen += 1;
+
+                }
+                else
+                {
+                    if (maxlen > answer)
+                    {
+                        answer = maxlen;
+                    }
+                    maxlen = 0;
+                }
+            }
+
+            return answer;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinaryGap.Solution(10));
+            Console.ReadKey();
+        }
+    }
+}
